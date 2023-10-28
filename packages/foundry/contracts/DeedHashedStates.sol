df47da06c791c899bdffa3b2ce5311cd5ab5b0dd
Pattisier <Principursa@protonmail.com>
2023-10-28 13:06:03 -0400

    Init

---
+++ packages/foundry/contracts/DeedHashedStates.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract DeedHashedStates {
+
+    enum TokenState {
+      InitialSetup,
+      InitialDocuments,
+      TitleAndEscrow,
+      InspectionsAndDisclosures,
+      ClosingStatements,
+      Payment,
+      DeedAndFinalDocuments,
+      Complete,
+      Cancelled
+    }
+
+}
---
+++ packages/foundry/contracts/LoanProtocol.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+// Useful for debugging. Remove when deploying to a live network.
+import "forge-std/console.sol";
+import "openzeppelin/"
+
+contract LoanProtocol {
+    struct LoanTerms {
+        uint interestRate;
+
+    }
+    mapping (address=>LoanTerms) testmap;
+}
